bf99e23cbf7a4ce5c2373fdcd24ca6afa9e19491
Yang Chih Wei <Blaze@Yangteki-MacBook-Pro.local>
2011-12-21 16:24:39 +0800

    Initial Commit

---
+++ QRCodeSacnner/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  QRCodeSacnner
+//
+//  Created by Chih Wei Yang on 11/12/21.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ QRCodeSacnner/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  QRCodeSacnner
+//
+//  Created by Chih Wei Yang on 11/12/21.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
